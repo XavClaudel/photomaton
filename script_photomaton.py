62295932aa2fdc7bb8f1fbeb9244393597837451
Xavier <xavclaudel88@hotmail.com>
2024-07-17 19:15:54 +0200

    feat/refonte_code

--- script_photomaton.py
+++ script_photomaton.py
@@ -1,161 +1,180 @@
-#!/usr/local/bin/python3
-# -*- coding: utf-8 -*-
 import pygame
-from pygame.locals import *
 import time
 import os
 import sys
-import RPi.GPIO as GPIO, time
-#d�éclaration et initialisation des ports GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
-
-def timer () :
-	#Création de la fenêtre 5
-	fenetre = pygame.display.set_mode((720, 420), pygame.FULLSCREEN)
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/5.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-	#Pause
-	time.sleep (1)
-	#Création de la fenêtre 4
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/4.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-	#Pause
-	time.sleep (1)
-	#Création de la fenêtre 3
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/3.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-	#Pause
-	time.sleep (1)
-	#Création de la fenêtre 2
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/2.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-	time.sleep (1)
-	#Création de la fenêtre 1
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/1.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-	#Pause
-	time.sleep (1)
-	#Création de la fenêtre 0
-	decompte = pygame.image.load("/home/pi/Documents/photomaton/images/0.jpg").convert()
-	fenetre.blit(pygame.transform.scale(decompte, (720, 420)), (0, 0))
-	pygame.display.flip()
-		 
-def creationdossierdroit () :
-	if (os.path.isdir("/media/pi/PHOTO/photos") == False): # si le dossier pour stocker les photos n'existe pas       
-		os.mkdir("/media/pi/PHOTO/photos")                  # alors on crée le dossier 
-		os.chmod("/media/pi/PHOTO/photos",0o777)            # et on change les droits pour pouvoir effacer des photos
-	if (os.path.isdir("/media/pi/PHOTO/photos/droits") == False): # si le dossier pour stocker les photos n'existe pas       
-		os.mkdir("/media/pi/PHOTO/photos/droits")                  # alors on crée le dossier 
-		os.chmod("/media/pi/PHOTO/photos/droits",0o777)            # et on change les droits pour pouvoir effacer des photos
-	
-def creationdossierpasdroit () :
-	if (os.path.isdir("/media/pi/PHOTO/photos") == False): # si le dossier pour stocker les photos n'existe pas       
-		os.mkdir("/media/pi/PHOTO/photos")                  # alors on crée le dossier 
-		os.chmod("/media/pi/PHOTO/photos",0o777)            # et on change les droits pour pouvoir effacer des photos
-	if (os.path.isdir("/media/pi/PHOTO/photos/pasdroits") == False): # si le dossier pour stocker les photos n'existe pas       
-		os.mkdir("/media/pi/PHOTO/photos/pasdroits")                  # alors on crée le dossier 
-		os.chmod("/media/pi/PHOTO/photos/pasdroits",0o777)            # et on change les droits pour pouvoir effacer des photos
 
 
-#Initialisation de la bibliothèque Pygame
+def timer():
+    fenetre = pygame.display.set_mode((640, 480))
+    for i in range(5, -1, -1):
+        decompte = pygame.image.load(f"images/{i}.jpg").convert()
+        fenetre.blit(pygame.transform.scale(decompte, (640, 480)), (0, 0))
+        pygame.display.flip()
+        time.sleep(1)
+
+
+def creationdossier(sous_chemin):
+    home_dir = os.getenv("HOME")  # Récupérer la variable d'environnement HOME
+    if home_dir is None:
+        raise EnvironmentError("La variable d'environnement HOME n'est pas définie.")
+
+    dir_path = os.path.join(home_dir, sous_chemin)  # Chemin complet du dossier
+
+    if not os.path.exists(dir_path):  # Vérifier si le dossier n'existe pas
+        os.makedirs(dir_path)  # Créer le dossier
+        print(f"Le dossier '{dir_path}' a été créé.")
+    else:
+        print(f"Le dossier '{dir_path}' existe déjà.")
+
+# Appels de la fonction pour chaque cas
+
+
+def draw_toggle_switch(screen, position, state):
+    pygame.draw.rect(screen, (255, 255, 255), (*position, 60, 30), border_radius=15)
+    if state:
+        pygame.draw.circle(screen, (0, 255, 0), (position[0] + 45, position[1] + 15), 15)
+    else:
+        pygame.draw.circle(screen, (255, 0, 0), (position[0] + 15, position[1] + 15), 15)
+
+def show_popup(screen, toggles):
+    font = pygame.font.Font(None, 50)
+    screen.fill((0, 0, 0))
+    params = ["Paramètre 1", "Paramètre 2", "Paramètre 3"]
+    for i, param in enumerate(params):
+        text = font.render(param, 1, (255, 255, 255))
+        screen.blit(text, (100, 100 + i * 100))
+        draw_toggle_switch(screen, (400, 100 + i * 100), toggles[i])
+    pygame.display.flip()
+
+def bienvenue():
+    droit_a_l_image = False
+    pygame.init()
+    fenetre = pygame.display.set_mode((640, 480))
+    font = pygame.font.Font(None, 120)
+    font1 = pygame.font.Font(None, 50)
+    icon = pygame.image.load("images/settings.png")  # Assurez-vous d'avoir une icône nommée "icon.png"
+    icon_rect = icon.get_rect(topleft=(600, 0))
+
+    running = True
+    show_popup_window = False
+    toggles = [False, False, False]  # Initial states of the toggle switches
+
+    while running:
+        fenetre.fill((0, 0, 0))
+        if droit_a_l_image:
+            text = font.render("Bienvenue", 1, (255, 255, 255))
+            text1 = font1.render("Appuyer sur le bouton vert", 1, (255, 255, 255))
+            text2 = font1.render("pour céder votre droits à l'image", 1, (255, 255, 255))
+            text3 = font1.render("Sinon appuyer sur le bouton rouge", 1, (255, 255, 255))
+            fenetre.blit(text, (100, 0))
+            fenetre.blit(text1, (15, 150))
+            fenetre.blit(text2, (15, 200))
+            fenetre.blit(text3, (15, 300))
+
+        fenetre.blit(icon, icon_rect)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if icon_rect.collidepoint(event.pos):
+                    show_popup_window = not show_popup_window
+                elif show_popup_window:
+                    for i in range(len(toggles)):
+                        toggle_rect = pygame.Rect(400, 100 + i * 100, 60, 30)
+                        if toggle_rect.collidepoint(event.pos):
+                            toggles[i] = not toggles[i]
+
+        if show_popup_window:
+            show_popup(fenetre, toggles)
+
+def init():
+    os.environ.get("DROIT_A_L_IMAGE", False)
+    os.environ.get("PRINT", False)
+    os.environ.get("DOWNLOAD", False)
+    # création des dossiers
+    creationdossier("documents/photos/droit_a_l_image")
+    creationdossier("documents/photos/pas_droit_a_l_image")
+    creationdossier("documents/tmp")
+    # montage de la clés
+
+
+# TODO settings
+# def settings():
+# 	#mise en place de la demande de droit à l'image
+# 	# impression
+# 	#téléchargement
+
+def print_image():
+    pass
+
+def download():
+    pass
+
+def affichage (path) :
+	#affichage de l'image
+	#fenetre = pygame.display.set_mode((640, 480), pygame.FULLSCREEN)
+    fenetre = pygame.display.set_mode((640, 480))
+    affichage = pygame.image.load(f"{path}").convert()
+    fenetre.blit(pygame.transform.scale(affichage, (640, 480)), (0, 0))
+    pygame.display.flip()
+    time.sleep (5)
+
 pygame.init()
-#Création et affichage de la fenêtre bienvenue
-def bienvenue () :
-	#création de la fenêtre de bienvenue
-	fenetre = pygame.display.set_mode((720, 420),pygame.FULLSCREEN)
-	#configuration des fonts
-	font=pygame.font.Font(None, 120)
-	font1=pygame.font.Font(None,60)
-	# configuration première ligne de texte
-	text = font.render("Bienvenue",1,(255,255,255))
-	#configuration deuxième ligne de texte
-	text1 = font1.render("Appuyer sur le bouton 1",1, (255,255,255))
-	#configration troisième ligne de texte
-	text2 = font1.render(u"pour céder votre droits à l'image",1, (255,255,255))
-	#configuration quatrième ligne de texte
-	text3 = font1.render("Sinon appuyer sur le bouton 2",1, (255,255,255))
-	#affichage première ligne de texte
-	fenetre.blit(text, (160,0))
-	#affichage deuxième ligne de texte
-	fenetre.blit(text1, (0,150))
-	#affichage troisième ligne de texte
-	fenetre.blit(text2, (0,200))
-	#affichage quatrième ligne de texte
-	fenetre.blit(text3, (0,300))
-	pygame.display.flip()
-# Création et affichage de la fenêtre de remerciement
-def merci () :
-	#création fenêtre de remerciement
-	fenetre=pygame.display.set_mode((720,420),pygame.FULLSCREEN)
-	#configuration font
-	font=pygame.font.Font (None , 120)
-	#configuration du texte
-	text = font.render ("Merci",1, (255,255,255))
-	#affichage du texte
-	fenetre.blit (text,  (250,150))
-	pygame.display.flip ()
-	#Pause
-	time.sleep (3)
-bienvenue ()
-if (os.path.isdir("/media/pi/PHOTO") == False): # si le dossier pour stocker les photos n'existe pas       
-		os.mkdir("/media/pi/PHOTO")                  # alors on crée le dossier 
-		os.chmod("/media/pi/PHOTO",0o777)
+init()
+bienvenue()
 while True:
-# Si commande via le GPIO décommenter la partie 1, si commande via le clavier décommenter la partie 2
-#Partie 1
-	# ecoute du GPIO2
-	etat=GPIO.input (2)
-	# ecoute du GPIO 3
-	etat2=GPIO.input(3)
-	if (etat==0):
-		timer ()
-		#montage du volume sur /media/pi/PHOTO
-		os.system ("mount /dev/sda1 /media/pi/PHOTO")
-		creationdossierdroit ()
-		# prise de photo
-		os.system("gphoto2 --capture-image-and-download --filename /media/pi/PHOTO/photos/droits/capt_%y_%m_%d-%H_%M_%S.jpg")
-		merci ()
-		# démonter le volume monter sur /media/pi/PHOTO
-		os.system("umount -f /media/pi/PHOTO")				
-		bienvenue ()
-	if (etat2==0):
-		timer ()
-		# montage du volume sur /media/pi/PHOTO
-		os.system ("mount /dev/sda1 /media/pi/PHOTO")
-		creationdossierpasdroit ()
-		#prise de photo
-		os.system("gphoto2 --capture-image-and-download --filename /media/pi/PHOTO/photos/pasdroits/capt_%y_%m_%d-%H_%M_%S.jpg")
-		merci()
-		#démontage du volume monter sur /media/pi/PHOTO
-		os.system("umount -f /media/pi/PHOTO")
-		bienvenue ()
-else:
-#Partie 2
-#	for event in pygame.event.get():    #Attente des événements
-			
+    # retour écran
+    for event in pygame.event.get():  # Attente des événements
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:  # Si "a" est  préssée
+                home = f"{os.environ.get("HOME")}/documents"
+                timer()
+                os.system(
+                    f"gphoto2 --capture-image-and-download --filename {os.environ.get('HOME')}/documents/tmp/capt_%y_%m_%d-%H_%M_%S.jpg"
+                )
+                if os.environ.get("DROIT_A_L_IMAGE"):
+                    # on copie l'image dans le dossier droit
+                    os.system(f"cp {home}/tmp/*jpg {home}/photos/pas_droit_a_l_image")
+                else:
+                    os.system(f"cp {home}/tmp/*jpg {home}/photos/droit_a_l_image")
 
-#			if event.type == KEYDOWN:
 
-				#if event.key == K_a: #Si "a" est  préssé 
-	#			
-	#				timer ()
-	#				creationdossierdroit ()
-	#				#prise de photo
-	#				os.system("gphoto2 --capture-image-and-download --filename /media/pi/PHOTO/photos/droits/capt_%y_%m_%d-%H_%M_%S.jpg")				
-	#							
-	#			if event.key == K_z: #Si "z" est préssé
-	#				timer ()
-	#				creationdossierpasdroit ()
-					#prise de photo
-	#				os.system("gphoto2 --capture-image-and-download --filename /media/pi/PHOTO/photos/pasdroits/capt_%y_%m_%d-%H_%M_%S.jpg")
-				
-			#	if event.key == K_e : #Si "e" est préssé
-			#		#quitter les fenêtres
-					pygame.quit ()
+                affichage(f"{home}/tmp/{os.listdir(f"{home}/tmp/")[0]}")
+                os.system(f" rm {home}/tmp/*jpg")
+                #Afficher la photo 
+                if  os.environ.get("PRINT"):
+                    print_image()
+                    pass
+   
+                if  os.environ.get("DOWNLOAD"):
+                    pass
+                    #genration qrcode
+                
+                bienvenue()
+
+
+
+            # if event.key == pygame.K_z: #Si "z" est préssé
+            # 	timer ()
+            # 	os.system ("mount /dev/sda1 /media/pi/PHOTO")
+            # 	creationdossierpasdroit ()
+            # 	#prise de photo
+            # 	os.system("gphoto2 --capture-image-and-download --filename /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/tmp/capt_%y_%m_%d-%H_%M_%S.jpg")
+            # 	#on copie l'image dans le dossier pas droits
+            # 	os.system ("cp /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/tmp/*jpg /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/pasdroits")
+            # 	#on renomme l'image
+            # 	os.system ("mv /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/tmp/*jpg /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/tmp/1.jpg")
+            # 	fenetre = pygame.display.set_mode((640, 480), pygame.FULLSCREEN)
+            # 	affichage ()
+            # 	time.sleep (3)
+            # 	#démontage de la clé
+            # 	os.system ("umount /media/pi/PHOTO")
+            # 	#on supprime l'image du fichier tmp
+            # 	os.system (" rm /home/xav/Nextcloud/dev/photomaton/script_affichage/ordi/avec_choix_droit_a_l_image/test/photos/tmp/1.jpg")
+            # 	merci ()
+            # 	bienvenue ()
+            if event.key == pygame.K_e:  # Si "e" est préssé
+                # quitter les fenêtres
+                pygame.quit()
---
+++ scrpit_photomation_ordi.py
@@ -0,0 +1,317 @@
+import os
+import time
+import pygame
+import sys
+import pyudev
+import subprocess
+import qrcode
+from concurrent.futures import ThreadPoolExecutor
+
+# Initialisation de Pygame
+pygame.init()
+
+# Dimensions de la fenêtre
+WIDTH, HEIGHT = 640, 480
+fenetre = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Polices
+font_large = pygame.font.Font(None, 120)
+font_small = pygame.font.Font(None, 50)
+
+# Couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# Variables d'environnement
+env_vars = ["DROIT_A_L_IMAGE", "PRINT", "DOWNLOAD", "CLES_USB"]
+
+# États initiaux des boutons glissants
+toggles = [os.getenv(var) == "TRUE" for var in env_vars]
+
+
+def draw_toggle_switch(screen, position, state):
+    pygame.draw.rect(screen, WHITE, (*position, 60, 30), border_radius=15)
+    if state:
+        pygame.draw.circle(screen, GREEN, (position[0] + 45, position[1] + 15), 15)
+    else:
+        pygame.draw.circle(screen, RED, (position[0] + 15, position[1] + 15), 15)
+
+
+def draw_settings_screen(screen, toggles):
+    screen.fill(BLACK)
+    params = ["DROIT_A_L_IMAGE", "PRINT", "DOWNLOAD", "CLES_USB"]
+    for i, param in enumerate(params):
+        text = font_small.render(param, 1, WHITE)
+        screen.blit(text, (100, 100 + i * 100))
+        draw_toggle_switch(screen, (450, 100 + i * 100), toggles[i])
+    pygame.display.flip()
+
+
+def draw_welcome_screen(screen):
+    screen.fill(BLACK)
+    text = font_large.render("Bienvenue", 1, WHITE)
+    if os.environ.get("DROIT_A_L_IMAGE"):
+        text1 = font_small.render("Appuyer sur le bouton vert", 1, (255, 255, 255))
+        text2 = font_small.render(
+            "pour céder votre droits à l'image", 1, (255, 255, 255)
+        )
+        text3 = font_small.render(
+            "Sinon appuyer sur le bouton rouge", 1, (255, 255, 255)
+        )
+        screen.blit(text, (100, 0))
+        screen.blit(text1, (15, 150))
+        screen.blit(text2, (15, 200))
+        screen.blit(text3, (15, 300))
+    else:
+        text1 = font_small.render("Appuyer sur le déclencheur", 1, (255, 255, 255))
+        text2 = font_small.render("pour prendre une photo", 1, (255, 255, 255))
+        screen.blit(text, (100, 0))
+        screen.blit(text1, (15, 150))
+        screen.blit(text2, (15, 200))
+
+    pygame.display.flip()
+
+
+def set_environment_variable(index, state):
+    os.environ[env_vars[index]] = "TRUE" if state else "FALSE"
+
+
+def timer():
+    fenetre = pygame.display.set_mode((640, 480))
+    for i in range(5, -1, -1):
+        decompte = pygame.image.load(f"images/{i}.jpg").convert()
+        fenetre.blit(pygame.transform.scale(decompte, (640, 480)), (0, 0))
+        pygame.display.flip()
+        time.sleep(1)
+
+
+def init():
+    os.environ.get("DROIT_A_L_IMAGE", False)
+    os.environ.get("PRINT", False)
+    os.environ.get("DOWNLOAD", False)
+    os.environ.get("CLES_USB", False)
+    creationdossier("documents/photos/droit_a_l_image")
+    creationdossier("documents/photos/pas_droit_a_l_image")
+    creationdossier("documents/tmp")
+
+
+def print_image():
+    print("print")
+    time.sleep(5)
+
+
+def start_http_server(racine):
+    os.system(f"cd {racine} && python3 -m http.server 9999")
+
+
+def generate_qr_code(data="https://www.example.com", filename="qrcode.png"):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill="black", back_color="white")
+    img.save(filename)
+    print(f"QR Code generated and saved as {filename}")
+
+
+def download(home, path):
+    if os.environ.get("DROIT_A_L_IMAGE"):
+        racine = f"{home}/photos/droit_a_l_image"
+
+    else:
+        racine = f"{home}/photos/pas_droit_a_l_image"
+
+    
+    qr_data = "https://www.example.com"
+    qr_filename = "qrcode.png"
+    with ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(start_http_server, racine),
+            executor.submit(generate_qr_code, qr_data, qr_filename),
+        ]
+
+        for future in futures:
+            try:
+                future.result()
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+
+def creationdossier(sous_chemin):
+    home_dir = os.getenv("HOME")  # Récupérer la variable d'environnement HOME
+    if home_dir is None:
+        raise EnvironmentError("La variable d'environnement HOME n'est pas définie.")
+
+    dir_path = os.path.join(home_dir, sous_chemin)  # Chemin complet du dossier
+
+    if not os.path.exists(dir_path):  # Vérifier si le dossier n'existe pas
+        os.makedirs(dir_path)  # Créer le dossier
+        print(f"Le dossier '{dir_path}' a été créé.")
+    else:
+        print(f"Le dossier '{dir_path}' existe déjà.")
+
+
+def creationdossier_usb(device_node, field):
+    home_dir = f"/media/{device_node.split('/')[2]}/{field}"  # Récupérer la variable d'environnement HOME
+    if home_dir is None:
+        raise EnvironmentError("La variable d'environnement HOME n'est pas définie.")
+
+    if not os.path.exists(home_dir):  # Vérifier si le dossier n'existe pas
+        os.makedirs(home_dir)  # Créer le dossier
+        print(f"Le dossier '{home_dir}' a été créé.")
+    else:
+        print(f"Le dossier '{home_dir}' existe déjà.")
+
+    return home_dir
+
+
+def affichage(path):
+    # affichage de l'image
+    fenetre = pygame.display.set_mode((640, 480))
+    affichage = pygame.image.load(path).convert()
+    fenetre.blit(pygame.transform.scale(affichage, (640, 480)), (0, 0))
+    pygame.display.flip()
+    time.sleep(5)
+
+
+def montage_cles_usb():
+    dir_path = f"/media/{os.environ.get('LOGNAME')}/PHOTOS"
+    if not os.path.exists(dir_path):  # Vérifier si le dossier n'existe pas
+        os.makedirs(dir_path)  # Créer le dossier
+    os.system(f"mount /dev/sdc1 {dir_path}")
+
+
+def mount_usb(device_node, mount_point):
+    """
+    Monte le périphérique USB.
+    :param device_node: Le noeud de périphérique (par exemple, /dev/sdb1).
+    :param mount_point: Le point de montage où monter le volume.
+    """
+    try:
+        os.makedirs(mount_point, exist_ok=True)
+        subprocess.run(["mount", device_node, mount_point], check=True)
+        print(f"Monté {device_node} sur {mount_point}")
+    except subprocess.CalledProcessError as e:
+        print(f"Erreur lors du montage de {device_node}: {e}")
+    except Exception as e:
+        print(f"Une erreur s'est produite: {e}")
+
+
+def monitor_usb(home):
+    """
+    Surveille les événements de connexion de périphériques USB et monte les nouveaux volumes.
+    """
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem="block", device_type="partition")
+
+    print("Surveillance des périphériques USB en cours...")
+
+    for device in iter(monitor.poll, None):
+        if device.action == "add":
+            device_node = device.device_node
+            mount_point = f"home/xav/documents/cles_usb_photos"
+            if not os.path.exists(mount_point):  # Vérifier si le dossier n'existe pas
+                os.makedirs(mount_point)  # Créer le dossier
+            print(f"Nouvelle partition détectée : {device_node}")
+            print(f"mount :{mount_point}")
+            os.system(f"pmount {device_node} ")
+            return device_node
+
+
+def main():
+    init()
+    running = True
+    in_welcome_screen = False
+    config_usb = 1
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z:
+                    in_welcome_screen = not in_welcome_screen
+            elif event.type == pygame.MOUSEBUTTONDOWN and not in_welcome_screen:
+                for i in range(len(toggles)):
+                    toggle_rect = pygame.Rect(450, 100 + i * 100, 60, 30)
+                    if toggle_rect.collidepoint(event.pos):
+                        toggles[i] = not toggles[i]
+                        set_environment_variable(i, toggles[i])
+
+        if in_welcome_screen:
+            draw_welcome_screen(screen=fenetre)
+            # print(os.environ)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        home = f"{os.environ.get("HOME")}/documents"
+                        if os.environ.get("CLES_USB") and config_usb == 1:
+                            print("cles_usb")
+                            device_node = monitor_usb(home)
+                            path_usb_droit_a__l_image = creationdossier_usb(
+                                device_node, field="photos/droit_a_l_image"
+                            )
+                            path_usb_pas_droit_a__l_image = creationdossier_usb(
+                                device_node, field="photos/pas_droit_a_l_image"
+                            )
+
+                            config_usb += config_usb
+                            print(config_usb)
+
+                        timer()
+                        os.system(
+                            f"gphoto2 --capture-image-and-download --filename {os.environ.get('HOME')}/documents/tmp/capt_%y_%m_%d-%H_%M_%S.jpg"
+                        )
+                        if os.environ.get("DROIT_A_L_IMAGE"):
+                            # on copie l'image dans le dossier droit
+                            os.system(
+                                f"cp {home}/tmp/*jpg {home}/photos/pas_droit_a_l_image"
+                            )
+                            if os.environ.get("CLES_USB"):
+                                os.system(
+                                    f"cp {home}/tmp/*jpg {path_usb_pas_droit_a__l_image}"
+                                )
+
+                        else:
+                            os.system(
+                                f"cp {home}/tmp/*jpg {home}/photos/droit_a_l_image"
+                            )
+                            if os.environ.get("CLES_USB"):
+                                os.system(
+                                    f"cp {home}/tmp/*jpg {path_usb_droit_a__l_image}"
+                                )
+
+                        path = f"{home}/tmp/{os.listdir(f"{home}/tmp/")[0]}"
+                        affichage(path=path)
+                        print("fin d'affichage")
+                        os.system(f" rm {home}/tmp/*jpg")
+                        # Afficher la photo
+                        if os.environ.get("PRINT"):
+                            print_image()
+                            pass
+
+                        if os.environ.get("DOWNLOAD"):
+                            download(home, path)
+                            # genration qrcode
+
+                        draw_welcome_screen(screen=fenetre)
+
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+        else:
+            draw_settings_screen(screen=fenetre, toggles=toggles)
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
