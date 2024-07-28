0e6d86dee24ffea5f3eed6da0fe9898ebac83ba4
Xavier <xavclaudel88@hotmail.com>
2024-07-28 07:45:24 +0200

    feat retour affichage

--- scrpit_photomation_raspberry.py
+++ scrpit_photomation_raspberry.py
@@ -245,7 +245,7 @@ def afficher_retour_video(screen:pygame):
         screen_width, screen_height = screen.get_size()
         # Convertir l'image de BGR (OpenCV) à RGB (Pygame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (screen_width, screen_height))
+        frame = cv2.resize(frame, (screen_height,screen_width))
         # Convertir l'image à un format que Pygame peut afficher
         print(f"frame:{frame}")
         frame = pygame.surfarray.make_surface(frame)
